5387204b519d26adf11497acf56562e0bcfb6845
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-10-15 13:18:49 +0530

    ✨ Writing Trainer Schems & Data

--- DAY2/GraphQL/Basics/schema/resolvers.js
+++ DAY2/GraphQL/Basics/schema/resolvers.js
@@ -1,7 +1,77 @@
 const listofcourses = [
-  { id: 1, title: "React", price: 5000 },
-  { id: 2, title: "Redux", price: 3000 },
+  {
+    id: 1,
+    title: "React",
+    price: 5000,
+    likes: 400,
+    rating: 5,
+    imageUrl:
+      "https://ms314006.github.io/static/b7a8f321b0bbc07ca9b9d22a7a505ed5/97b31/React.jpg",
+    trainerId: 1,
+  },
+  {
+    id: 2,
+    title: "Redux",
+    price: 4000,
+    likes: 600,
+    rating: 5,
+    imageUrl: "https://logicalidea.co/wp-content/uploads/2020/05/Redux.jpg",
+    trainerId: 2,
+  },
+  {
+    id: 3,
+    title: "Node",
+    price: 6000,
+    likes: 900,
+    rating: 4,
+    imageUrl:
+      "https://www.cloudsavvyit.com/p/uploads/2019/07/2350564e.png?width=1198&trim=1,1&bg-color=000&pad=1,1",
+    trainerId: 1,
+  },
+  {
+    id: 4,
+    title: "Angular",
+    price: 5000,
+    likes: 200,
+    rating: 3,
+    imageUrl:
+      "https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/15991/top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png",
+    trainerId: 3,
+  },
+  {
+    id: 5,
+    title: "Flutter",
+    price: 7000,
+    likes: 700,
+    rating: 4,
+    imageUrl: "https://miro.medium.com/max/2000/1*PCKC8Ufml-wvb9Vjj3aaWw.jpeg",
+    trainerId: 3,
+  },
 ];
+const trainerList = [
+  {
+    id: 1,
+    name: "John Carter",
+    isMCT: true,
+    followers: 100,
+    avatarUrl: "https://avatars.githubusercontent.com/u/1?v=4",
+  },
+  {
+    id: 2,
+    name: "John Mojombo",
+    isMCT: true,
+    followers: 700,
+    avatarUrl: "https://avatars.githubusercontent.com/u/2?v=4",
+  },
+  {
+    id: 3,
+    name: "Allen Defunkt",
+    isMCT: true,
+    followers: 400,
+    avatarUrl: "https://avatars.githubusercontent.com/u/3?v=4",
+  },
+];
+
 export const resolvers = {
   Query: {
     hello: () => "Hello GraphQL !",
@@ -11,5 +81,7 @@ export const resolvers = {
       return listofcourses;
     },
     course: (_, { id }) => listofcourses.find(c => c.id == id),
+    trainers: () => trainerList,
+    trainer: (_, args) => trainerList.find(trainer => trainer.id == args.id),
   },
 };
--- DAY2/GraphQL/Basics/schema/typedefs.js
+++ DAY2/GraphQL/Basics/schema/typedefs.js
@@ -3,11 +3,28 @@ type Query{
     hello:String
     courses:[Course]
     course(id:ID!):Course
+    trainers:[Trainer]
+    trainer(id:ID!):Trainer
 }
 
 type Course{
     id:ID
     title:String
     price:Int
+    rating:Int
+    likes:Int
+    imageUrl:String
+    trainer:Trainer
 }
+ type Trainer{
+        id:ID!
+        name:String!
+        isMCT:Boolean
+        avatarUrl:String
+        followers:Int
+    }
 `;
+// new Type Trainer -> Trainerid, name,isMCT,avatarUrl
+// // type Query -> add -> trainers, trainer(id)
+
+// rating , imageUrl , likes
